49421319a2db41a7da770f758dfc957bcf6b1405
rnl <rnl@cesar.school>
2026-08-23 10:59:01 -0300

    adicionando o codigo com paralelismo estruturado

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,81 @@
 
     private static int totalTarefas = 4;
 
-    private static double tarefaPesadaVariavelEficiente() {
+    private static double tarefaPesadaVariavelEficiente(int numeroTafera, boolean paralelo) {
         String nomeAtual = Thread.currentThread().getName();
         System.out.println("Rodando na thread: " + nomeAtual);
         double soma = 0;
 
+        if (numeroTafera == 2 && paralelo) {
+            throw new RuntimeException(
+                    "Erro na tarefa " + numeroTafera
+            );
+        }
+
         for (int i = 0; i < 100000000; i++) {
+            if (Thread.currentThread().isInterrupted()) {
+                return 0;
+            }
                 soma += i;
         }
 
         return soma;
     }
 
-    private static Double Calculate()
+    private static Double SequencialCalculate()
     {
         double somaTotal = 0.0;
         for (int i = 0; i < totalTarefas; i++) {
-            somaTotal = somaTotal + tarefaPesadaVariavelEficiente();
+            somaTotal = somaTotal + tarefaPesadaVariavelEficiente(i, false);
         }
         return somaTotal;
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    private static double SomaParelelaEstruturada()
+            throws InterruptedException {
+
+        double somaTotal = 0.0;
+
+        try (var scope = StructuredTaskScope.open()) {
 
 
-        System.out.println("Executando " + totalTarefas + " tarefas pesadas...\n");
+            Supplier<Double>[] tarefas = new Supplier[totalTarefas];
 
-        // ==========================================
-        // 1. EXECUÇÃO SEQUENCIAL
-        // ==========================================
-        long inicioSeq = System.currentTimeMillis();
-        double somaTotal = 0;
-        somaTotal = Calculate();
+            for (int i = 0; i < totalTarefas; i++) {
 
-        long tempoSeq = System.currentTimeMillis() - inicioSeq;
-        System.out.println("Soma Total: " + somaTotal);
-        System.out.println("Tempo Sequencial: " + tempoSeq + " ms");
+                final int numeroTarefa = i;
+                tarefas[i] = scope.fork(() ->
+                        tarefaPesadaVariavelEficiente(numeroTarefa, true));
+            }
 
-        System.out.println("------------------------------------------------------");
-        // ==========================================
-        // 2. EXECUÇÃO PARALELA (Usando Thread)
-        // ==========================================
+            System.out.println("Todas as tarefas foram iniciadas.");
 
-        long inicioPar = System.currentTimeMillis();
+            scope.join();
 
+            for (Supplier<Double> tarefa : tarefas) {
+                somaTotal += tarefa.get();
+            }
+
+            System.out.println(
+                    "Todas as tarefas terminaram com sucesso."
+            );
+
+        } catch (StructuredTaskScope.FailedException e) {
+
+            System.out.println("\n!!! UMA TAREFA FALHOU !!!");
+
+            System.out.println("Erro: " + e.getCause().getMessage());
+
+            somaTotal = 0.0;
+
+            System.out.println("O escopo foi encerrado.");
+        }
+
+        return somaTotal;
+    }
+
+    public static Double SomaParalelaNaoEstruturada()
+    {
         List<CompletableFuture<Double>> futures =
                 new ArrayList<>();
 
@@ -55,14 +86,7 @@
 
             CompletableFuture<Double> future =
                     CompletableFuture.supplyAsync(() -> {
-
-                        if (numeroTarefa == 2) {
-                            throw new RuntimeException(
-                                    "Erro na tarefa " + numeroTarefa
-                            );
-                        }
-
-                        return tarefaPesadaVariavelEficiente();
+                        return tarefaPesadaVariavelEficiente(numeroTarefa, true);
                     });
 
             futures.add(future);
@@ -81,10 +105,37 @@
             }
         }
 
-        long tempoPar = System.currentTimeMillis() - inicioPar;
         System.out.println("Soma Total: " + somaTotalParalela);
-        System.out.println("Tempo Paralelo:   " + tempoPar + " ms\n");
+        return somaTotalParalela;
+    }
 
-        // Resultado
-        System.out.printf("O código paralelo foi %.1fx mais rápido!%n", (double) tempoSeq / tempoPar);
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Executando " + totalTarefas + " tarefas pesadas...\n");
+        long inicioSeq = System.currentTimeMillis();
+        double somaTotal = 0;
+        somaTotal = SequencialCalculate();
+
+        long tempoSeq = System.currentTimeMillis() - inicioSeq;
+        System.out.println("Soma Total Sequencial: " + somaTotal);
+        System.out.println("Tempo Sequencial: " + tempoSeq + " ms");
+
+        System.out.println("------------------------------------------------------");
+
+        long inicioPar = System.currentTimeMillis();
+
+        somaTotal = SomaParalelaNaoEstruturada();
+
+        long tempoPar = System.currentTimeMillis() - inicioPar;
+        System.out.println("Soma Total paralela nao estruturada: " + somaTotal);
+        System.out.println("Tempo Paralelo nao estruturado:   " + tempoPar + " ms\n");
+
+        inicioPar = System.currentTimeMillis();
+
+        somaTotal = SomaParelelaEstruturada();
+
+        tempoPar = System.currentTimeMillis() - inicioPar;
+        System.out.println("Soma Total paralela estruturada: " + somaTotal);
+        System.out.println("Tempo Paralelo estruturado:   " + tempoPar + " ms\n");
+
     }
