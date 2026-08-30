4fe870287b17e13519862c87286153ea1ebb22a4
rnl <rnl@cesar.school>
2026-08-30 12:35:04 -0300

    Adicionando codigo de variavel compartilhada

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,24 @@
 
-    // Simula um trabalho pesado de CPU
-    private static void tarefaPesada() {
-        double soma =0;
-        String nomeAtual = Thread.currentThread().getName();
-        System.out.println("Rodando na thread: " + nomeAtual);
-        for (int i = 0; i < 5000; i++) {
-            soma += Math.sqrt(i);
-        }
-
-        System.out.println("Soma total " + soma );
-    }
+private static int valor = 0;
 
     public static void main(String[] args) throws InterruptedException {
-        int totalTarefas = 4;
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100_000; i++) {
+                valor++;
+            }
+        });
 
-        System.out.println("Executando " + totalTarefas + " tarefas pesadas...\n");
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100_000; i++) {
+                valor++;
+            }
+        });
 
-        // ==========================================
-        // 1. EXECUÇÃO SEQUENCIAL
-        // ==========================================
-        long inicioSeq = System.currentTimeMillis();
+        t1.start();
+        t2.start();
 
-        for (int i = 0; i < totalTarefas; i++) {
-            tarefaPesada();
-        }
+        t1.join();
+        t2.join();
 
-        long tempoSeq = System.currentTimeMillis() - inicioSeq;
-        System.out.println("Tempo Sequencial: " + tempoSeq + " ms");
-
-        // ==========================================
-        // 2. EXECUÇÃO PARALELA (Usando Thread)
-        // ==========================================
-        long inicioPar = System.currentTimeMillis();
-        Thread[] threads = new Thread[totalTarefas];
-
-        // Criando e iniciando as threads
-        for (int i = 0; i < totalTarefas; i++) {
-            threads[i] = new Thread(() -> tarefaPesada());
-            threads[i].start(); // Dispara a execução em paralelo
-        }
-
-        // Aguardando todas as threads terminarem
-        for (int i = 0; i < totalTarefas; i++) {
-            threads[i].join(); // Pausa a thread principal até que a thread 'i' finalize
-        }
-
-        long tempoPar = System.currentTimeMillis() - inicioPar;
-        System.out.println("Tempo Paralelo:   " + tempoPar + " ms\n");
-
-        // Resultado
-        System.out.printf("O código paralelo foi %.1fx mais rápido!%n", (double) tempoSeq / tempoPar);
+        System.out.println(valor);
     }
