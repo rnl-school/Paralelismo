585460c58506372426e0d256ce61d7974d097320
rnl <rnl@cesar.school>
2026-08-31 19:55:15 -0300

    adicionando o valor da variavel compartilhada em uma funcao

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 
 private static int valor = 0;
 
+    private synchronized static void incrementar()
+    {
+        valor++;
+    }
+
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 100_000; i++) {
-                valor++;
+                incrementar();
             }
         });
 
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 100_000; i++) {
-                valor++;
+                incrementar();
             }
         });
 
